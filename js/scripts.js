7d3b161d010b06534ad7244117d00f9b1ec0d8f3
chryswoehler <128384592+chryswoehler@users.noreply.github.com>
2024-10-24 12:27:58 -0400

    Added basic files
    base files set up for the main, settings, and faq page. Very rough formatting still

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function login() {
+    // Simulates a login process
+    window.location.href = 'main.html';  // Redirect to main page after login
+}
+
+function navigateToSettings() {
+    window.location.href = 'settings.html';  // Redirect to settings page
+}
+
+function navigateToFAQ() {
+    window.location.href = 'faq.html';  // Redirect to faq page
+}
