04e5fede3f2bc72d67d358ca4f0453e616d452de
chryswoehler <128384592+chryswoehler@users.noreply.github.com>
2024-11-13 00:21:41 -0500

    Edits to add pop up

--- main.php
+++ main.php
@@ -60,23 +60,23 @@ $user_meds = $db->query(
 	</div>
 	<div class="container-fluid">
 	<!-- set up bootstrap grid system to make page format responsive -->
-	<div class="row">
-		<div class="col-4 d-flex flex-column align-items-center">
+	<div class="row flex-nowrap">
+		<div class="col-3 d-flex flex-column align-items-center">
 			<img src="images/PillPartner.png" alt="PillPartner Avatar" class="pill-partner text-start text m-3" style="width: 30vw; height: 30vw">
 			<!-- Button to Open Modal -->
 				<button type="button" class="btn btn-primary med-button " data-bs-toggle="modal" data-bs-target="#addModal">
-					Add Medication
+					Add New Perscription
 				</button>
 			
 		</div>
-		<div class="col-7" style="background-color: lightblue; border-width: 5px; border-style: solid; border-color: white; border-radius: 10px; padding: 15px;">
+		<div class="col-8" style="background-color: lightblue; border-width: 5px; border-style: solid; border-color: white; border-radius: 10px; margin: 1rem; padding-right: 1rem;">
 			<p><strong>Logged in as:</strong> <?php echo $_SESSION['UserFirstName']." ".$_SESSION['UserLastName']. " (".$_SESSION['UserName'].")"; ?></p>
-			<table class="table table-bordered custom-table">
+			<table class="table table-bordered" style="width: 100%; ">
 			<thead class="thead-dark">
 			<colgroup>
-				<col style="width: 20%;">
-				<col style="width: 30%;">
-				<col style="width: 20%;">
+				<col style="width: 9vw;">
+				<col style="width: 25%;">
+				<col style="width: 15%;">
 				<col style="width: 20%;">
 				<col style="width: 5%;">
 				<col style="width: 5%;">
@@ -101,7 +101,8 @@ $user_meds = $db->query(
 					echo "<td class=\"text-nowrap\">".$med["PrescriptionNotes"]."</td>";
                     echo "<td class=\"text-center\">TBD</td>";
 					echo "<td class=\"text-center\"><button 
-					class=\"btn btn-link p-0 m-0 text-decoration-none\" 
+					class=\"btn btn-link text-decoration-none\" 
+					style=\"padding: calc(1 rem + 0.05vw)\"
 					data-bs-toggle=\"modal\" 
 					data-bs-target=\"#editModal\"
 					data-medication=\"" . $med['MedicationName'] . "\"
@@ -115,8 +116,7 @@ $user_meds = $db->query(
 			  </tbody>
 			</table>
 		</div>
-		<div class="col-1">
-		</div>
+		
 	</div>
 	<div class="row">
 		<div class="col-8 text-center" style="margin-top: auto;">
@@ -143,13 +143,13 @@ $user_meds = $db->query(
 										<div class="col-4 text-end">
 											<label style="font-size: 2vw;" for="UserName"><b>Medication dosage: </b></label>
 										</div>
-										<div class="col-8 text-start text-wrap" style="display: flex; gap: 10px;">
+										<div class="col-8 d-flex flex-wrap" style="display: flex; gap: 10px;">
 											<label style="font-size: 1vw;"><b>Take </b></label>
 											<div class="form-group">
 												<input type="text" placeholder="Type..." name="medicationAmount" style="width: 4vw; height: 1.50vw; font-size: 1vw;" oninput="this.style.width = (this.value.length + 2) + 'ch'" required></input>	
 												</div>
 												<div class="form-group">
-												<select class="dynamicDropdown" id="firstDropdown" style="width: auto; font-size: 0.9vw;" onchange="resizeDropdown(this)" required>
+												<select class="dynamicDropdown" id="PrescriptionUnit" style="width: auto; font-size: 0.9vw;" onchange="resizeDropdown(this)" required>
 													<option value="" disabled selected>Select an amount</option>
 													<option value="1">mg</option>
 													<option value="2">ml</option>
@@ -226,30 +226,44 @@ $user_meds = $db->query(
 					<div class="modal-content">
 						<form>
 							<div class="modal-header">
-								<h1 class="modal-title" id="addModalLabel">Add New Medication</h2>
+								<h1 class="modal-title" id="addModalLabel">Add New Perscription</h2>
 								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 							</div>
 							<div class="modal-body">
 								<div class="container-fluid">
 										<div class="row mb-3">
 											<div class="col-4 text-end">
-												<label style="font-size: 2vw;" for="UserName"><b>Medication name: </b></label>
+												<label style="font-size: 2rem;" for="UserName"><b>Medication name: </b></label>
 											</div>
 											<div class="col-4 text-start">
-												<input type="text" placeholder="Type Name..." name="medicationName" style="width: 10vw; height: 2vw; font-size: 1vw;" required></input>
+												<div class="form-group">
+														<select class="medicationDropdown" id="PrescriptionUnit" style="width: auto; font-size: 0.9vw;" onchange="resizeDropdown(this)" required>
+															<option value="" disabled selected>Select an amount</option>
+															<option value="1">med1</option>
+															<option value="2">med2</option>
+															<option value="3">med3</option>
+															<option value="4">med4</option>
+														</select>
+												</div>
+												<button type="button" class="btn btn-primary custom-button" onclick="toggleCustomMed()">
+													Add New Medication
+												</button>
+												<div class="form-group" id="customMedication" style="display: none;">
+													<label style="font-size: 1rem;"><b>Take </b></label>
+												</div>
 											</div>
 										</div>
 										<div class="row mb-3">
 											<div class="col-4 text-end">
-											<label style="font-size: 2vw;" for="UserName"><b>Medication dosage: </b></label>
+											<label style="font-size: 2rem;" for="PerscirptionDosage"><b>Medication dosage: </b></label>
 											</div>
-											<div class="col-8 text-start text-wrap" style="display: flex; gap: 10px;">
-											<label style="font-size: 1vw;"><b>Take </b></label>
+											<div class="col-8 d-flex flex-wrap" style="display: flex; gap: 10px;">
+											<label style="font-size: 1rem;"><b>Take </b></label>
 													<div class="form-group">
 														<input type="text" placeholder="Type..." name="medicationAmount" style="width: 4vw; height: 1.50vw; font-size: 1vw;" oninput="this.style.width = (this.value.length + 2) + 'ch'" required></input>	
 													</div>
 													<div class="form-group">
-														<select class="dynamicDropdown" id="firstDropdown" style="width: auto; font-size: 0.9vw;" onchange="resizeDropdown(this)" required>
+														<select class="dynamicDropdown" id="PrescriptionUnit" style="width: auto; font-size: 0.9vw;" onchange="resizeDropdown(this)" required>
 															<option value="" disabled selected>Select an amount</option>
 															<option value="1">mg</option>
 															<option value="2">ml</option>
@@ -257,7 +271,7 @@ $user_meds = $db->query(
 															<option value="4">tablets</option>
 														</select>
 													</div>
-													<label style="font-size: 1vw;" for="UserPassword"><b>per </b></label>
+													<label style="font-size: 1rem;"><b>per </b></label>
 													<div class="form-group">
 														<select class="dynamicDropdown" id="secondDropdown" style="width: auto; font-size: 0.9vw;"onchange="toggleThirdDropdown(); resizeDropdown(this);" required>
 															<option value="" disabled selected>Select a frequency</option>
@@ -266,8 +280,8 @@ $user_meds = $db->query(
 															<option value="3">month</option>
 														</select>
 													</div>
-													<div class="form-group" id="thirdDropdown" style="display: none; font-size: 0.9vw;" >
-														<label style="font-size: 1vw;"><b> on </b></label>
+													<div class="form-group" id="DayDropDown" style="display: none; font-size: 0.9vw;" >
+														<label style="font-size: 1rem;"><b> on </b></label>
 														<select class="dynamicDropdown" id="thirdDropdownSelect" onchange="resizeDropdown(this)" required>
 															<option value="option1">Monday</option>
 															<option value="option2">Tuesday</option>
@@ -278,9 +292,9 @@ $user_meds = $db->query(
 															<option value="option2">Sunday</option>
 														</select>
 													</div>	
-													<label style="font-size: 1vw;"><b> at </b></label>
-													<input type="text" placeholder="Hr" name="hourValue" style="width: 2.50vw; height: 1.50vw; font-size: 1vw;" oninput="this.style.width = (this.value.length + 2) + 'ch'" required></input>	
-													<label style="font-size: 1vw;"><b>:</b></label>
+													<label style="font-size: 1rem;"><b> at </b></label>
+													<input type="text" style="width: 2.50vw; height: 1.50vw; font-size: 1vw; color: black;" placeholder="Hr" name="hourValue"  oninput="this.style.width = (this.value.length + 2) + 'ch'" required></input>	
+													<label style="font-size: 1rem;"><b>:</b></label>
 													<input type="text" placeholder="Min" name="minuteValue" style="width: 2.50vw; height: 1.50vw; font-size: 1vw;" oninput="this.style.width = (this.value.length + 2) + 'ch'" required></input>	
 													<div class="form-group">
 														<select class="dynamicDropdown" id="fourthDropdwon" style="width: auto; font-size: 0.9vw;"onchange="toggleThirdDropdown(); resizeDropdown(this);" required>
@@ -293,11 +307,11 @@ $user_meds = $db->query(
 										</div>
 										<div class="row mb-3">
 											<div class="col-4 text-end text-wrap">
-												<label style="font-size: 2vw;" for="UserPassword"><b>Image of medication:</b></label> 
+												<label style="font-size: 2rem;" for="UserPassword"><b>Image of medication:</b></label> 
 											</div>
 											<div class="col-4 text-start text-wrap">
 												<div class="form-group">
-													<label style="font-size: 1vw;" for="imageUpload">Select image to upload:</label>
+													<label style="font-size: 1rem;" for="imageUpload">Select image to upload:</label>
 													<input type="file" class="form-control-file" id="imageUpload" style="font-size: .8vw;" accept="image/*">
 												</div>
 												<button type="button" class="btn btn-primary" style="font-size: 1vw">Upload</button>
@@ -305,7 +319,7 @@ $user_meds = $db->query(
 										</div>
 										<div class="row mb-3">
 											<div class="col-4 text-end text-wrap">
-												<label style="font-size: 2vw;" for="UserPassword"><b>Notes:</b></label>
+												<label style="font-size: 2rem;" for="UserPassword"><b>Notes:</b></label>
 											</div>
 											<div class="col-4 text-wrap">
 												<input type="text" placeholder="Type here..." name="UserPassword" style="width: 30vw; height: 10vw; font-size: 1vw; padding-top: 0.5vw;"></input>	
@@ -338,60 +352,75 @@ $user_meds = $db->query(
 	<!-- Custom JavaScript -->
     <script src="js/scripts.js"></script>
 	<script> 
-		function toggleThirdDropdown() {
-			var secondDropdown = document.getElementById("secondDropdown");
-			var thirdDropdown = document.getElementById("thirdDropdown");
+	function toggleThirdDropdown() {
+		var secondDropdown = document.getElementById("secondDropdown");
+		var DayDropDown = document.getElementById("DayDropDown");
 
-			
-			// Show second dropdown if the specific option is selected
-			if (secondDropdown.value === "2") {
-				thirdDropdown.style.display = "block";
-			} else {
-				thirdDropdown.style.display = "none";
-			}
+		
+		// Show second dropdown if the specific option is selected
+		if (DayDropDown.style.display === "none" || DayDropDown.style.display === "") {
+			DayDropDown.style.display = "block";
+		} else {
+			DayDropDown.style.display = "none";
 		}
-	</script>
-	<script>
-		function resizeDropdown(dropdown){
-			let tempSpan = document.createElement("span");
-			tempSpan.style.visibilisty = "hidden";
-			tempSpan.style.position = "absolute";
-			tempSpan.style.whiteSpace = "nowrap";
-			tempSpan.innerText = dropdown.options[dropdown.selectedIndex].text;
-			document.body.appendChild(tempSpan);
-			
-			dropdown.style.width = `${tempSpan.offsetWidth + 30}px`; 
-			document.body.removeChild(tempSpan);
+	}
+</script>
+<script>
+	function resizeDropdown(dropdown){
+		let tempSpan = document.createElement("span");
+		tempSpan.style.visibility = "hidden";
+		tempSpan.style.position = "absolute";
+		tempSpan.style.whiteSpace = "nowrap";
+		tempSpan.innerText = dropdown.options[dropdown.selectedIndex].text;
+		document.body.appendChild(tempSpan);
+		
+		dropdown.style.width = `${tempSpan.offsetWidth + 30}px`; 
+		document.body.removeChild(tempSpan);
+	}
+</script>
+<script>
+	function resizeField(field){
+		let tempSpan = document.createElement("span");
+		tempSpan.style.visibility = "hidden";
+		tempSpan.style.position = "absolute";
+		tempSpan.style.whiteSpace = "nowrap";
+		tempSpan.innerText = field.options[field.selectedIndex].text;
+		document.body.appendChild(tempSpan);
+		
+		field.style.width = `${tempSpan.offsetWidth + 30}px`; 
+		document.body.removeChild(tempSpan);
+	}
+</script>
+<script>
+	function toggleCustomMed() {
+		var dropdown = document.getElementById("toggleCustomMed");
+
+		// Toggle between hiding and showing the dropdown menu
+		if (dropdown.style.display === "none" || dropdown.style.display === "") {
+			dropdown.style.display = "block";
+		} else {
+			dropdown.style.display = "none";
 		}
-	</script>
-	<script>
-		function resizeField(field){
-			let tempSpan = document.createElement("span");
-			tempSpan.style.visibilisty = "hidden";
-			tempSpan.style.position = "absolute";
-			tempSpan.style.whiteSpace = "nowrap";
-			tempSpan.innerText = field.options[field.selectedIndex].text;
-			document.body.appendChild(tempSpan);
-			
-			field.style.width = `${tempSpan.offsetWidth + 30}px`; 
-			document.body.removeChild(tempSpan);
-		}
-	</script>
-	<script>
-	document.addEventListener('DOMContentLoaded', function () {
-		const editModal = document.getElementById('editModal');
-		editModal.addEventListener('show.bs.modal', function (event) {
-			const button = event.relatedTarget; // Button that triggered the modal
-			const medication = button.getAttribute('data-medication');
-			const dosage = button.getAttribute('data-dosage');
-			const notes = button.getAttribute('data-notes');
-			
-			// Populate the modal fields
-			document.getElementById('medicationName').value = medication;
-			document.getElementById('dosage').value = dosage;
-			document.getElementById('notes').value = notes;
-		});
+	}
+</script>
+<script>
+	const editModal = document.getElementById('editModal');
+	editModal.addEventListener('show.bs.modal', function (event) {
+		const button = event.relatedTarget; // Button that triggered the modal
+		const medication = button.getAttribute('data-medication');
+		const dosage = button.getAttribute('data-dosage');
+		const notes = button.getAttribute('data-notes');
+		
+		// Populate the modal fields
+		document.getElementById('medicationName').value = medication;
+		document.getElementById('dosage').value = dosage;
+		document.getElementById('notes').value = notes;
 	});
-	</script>
+	const addModal = document.getElementById('addModal');
+	addModal.addEventListener('show.bs.modal', function (event) {			
+		document.getElementById("customMedication").addEventListener("click", toggleCustomMed);
+
+	});
+</script>
 </body>
 </html>
